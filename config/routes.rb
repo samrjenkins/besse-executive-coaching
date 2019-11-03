7320d391dd965f8e1f9318ceea9f8a49cde2d690
Sam Jenkins <sam.jenko@hotmail.com>
2019-11-03 16:06:09 +0000

    add Devise. Add coach model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :coaches
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :coach do
+    unauthenticated :coach do
+      root to: 'devise/sessions#new'
+    end
+
+    authenticated :coach do
+      get '/', to: redirect('/clients')
+      resources :clients
+      resources :events
+    end
+  end
 end
